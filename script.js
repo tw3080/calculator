872bdada2d639b7d442bb2ec031b96fb2372644c
tw3080 <tw3080@gmail.com>
2023-03-22 16:58:18 -0700

    Add feature that populates the calculator's display when each number or operator button is clicked

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const displayText = document.querySelector('#display-text');
+let displayValue = 0;
+let operatorValue = '';
+
 const add = function(a, b) {
     return a + b;
 }
@@ -17,3 +23,39 @@ const divide = function(a, b) {
 const operate = function(operator, a, b) {
     return operator(a, b);
 }
+
+const getNumber = function(button) {
+    displayValue = button.value;
+    displayText.textContent += displayValue;
+}
+
+const getOperator = function(button) {
+    operatorValue = button.value;
+    displayText.textContent += operatorValue;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        getNumber(button);
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        getOperator(button);
+    });
+});
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         displayValue = button.value;
+//         displayText.textContent += displayValue;
+//         console.log(displayValue);
+//     });
+// });
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', function(e) {
+//         console.log(e);
+//     });
+// });
